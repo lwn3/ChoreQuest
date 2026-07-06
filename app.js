302b54f2dc694130983f763d43e9aa3748d6c1ce
lwn3 <lawrencewnelson3@gmail.com>
2026-07-05 23:00:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -697,7 +697,7 @@ function checkAuthState() {
                         showError("Sign in failed: " + err.message);
                     });
             });
-    });
+    };
 })
 
 // --- INITIALIZATION ---
